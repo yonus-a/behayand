2cf3b505d6b7b85bf7265233ac6adc5878a85195
Amir Safari <a.safari1380@gmail.com>
2026-04-12 19:06:12 +0330

    fixed the issue related to the pin code

---
+++ app/pages/auth/profile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ImageUpload />
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ImageUpload from '~/components/general/ImageUpload.vue';
+
+export default defineComponent({
+    name: 'ProfilePage',
+    components: {
+        ImageUpload,
+    },
+    setup() {
+
+
+        return {
+
+        }
+    }
+})
+</script>
