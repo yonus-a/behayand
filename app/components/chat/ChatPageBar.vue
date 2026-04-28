7ce0354669c7e177b612ec7669d9d519ae30a2ab
Amir Safari <a.safari1380@gmail.com>
2026-04-28 10:56:32 +0330

    made the actions for the chat

--- app/components/chat/ChatPageBar.vue
+++ app/components/chat/ChatPageBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="selectedChat"
         class="w-full h-16 md:h-20 gap-x-4 bg-surface border-b border-b-outline-variant flex items-center justify-between py-4 px-5">
-        <div class=" w-full md:flex-row flex-row-reverse  justify-end gap-x-4 md:justify-between flex">
+        <div class=" w-full md:flex-row flex-row-reverse items-center justify-end gap-x-4 md:justify-between flex">
             <div @click="openProfile" class="cursor-pointer flex items-center gap-x-3">
                 <div class="w-10 h-10 relative shrink-0">
                     <ContactAvatar v-if="contact" :contact="contact" />
@@ -14,32 +14,44 @@
                 </div>
             </div>
 
-            <div class="flex items-center gap-x-4">
-                <div class="  hidden md:block">
-                    <BIcon icon="PhPhone" v-if="selectedChat.serviceType !== 'chat'"
-                        class="w-6 h-6 fill-on-surface/50 cursor-pointer" @click="handleAction('call')" />
-                </div>
-
-
-                <BMenu ref="menuRef">
-                    <template #trigger>
-                        <BIcon icon="PhDotsThreeVertical" class="w-6 h-6 fill-on-surface/50 cursor-pointer"
-                            aria-haspopup="true" aria-controls="overlay_menu" />
-                    </template>
-                    <div class=" flex flex-col gap-y-1 select-none w-60 p-3">
-                        <div @click="handleMenuAction(option.key)" v-for="option in menuOptions" :key="option.key"
-                            :class="[option.color && option.color === 'error' ? ' hover:bg-error/10 bg-error/0' : 'hover:bg-surface-variant-2 bg-surface-variant-2/0']"
-                            class=" cursor-pointer group  transition-all duration-200 ease-in-out rounded-xl w-full flex items-center px-2.5 py-3 gap-x-2">
-                            <BIcon
-                                :class="[option.color && option.color === 'error' ? 'fill-error' : 'fill-on-surface/50 group-hover:fill-on-surface']"
-                                class=" transition-all duration-200 ease-in-out w-5 h-5" weight="bold"
-                                :icon="option.icon" />
-                            <div class=" text-label-sm transition-all duration-200 ease-in-out"
-                                :class="[option.color && option.color === 'error' ? 'text-error' : 'text-on-surface/50 group-hover:text-on-surface']">
-                                {{ option.label }}</div>
+            <div class=" h-6">
+                <div class=" transition-all duration-200 ease-in-out"
+                    :class="[isSelectMode ? '-translate-y-6' : 'translate-y-0']">
+                    <div class="flex items-center gap-x-4 transition-all duration-200 ease-in-out"
+                        :class="[isSelectMode ? ' pointer-events-none opacity-0' : ' opacity-100 pointer-events-auto']">
+                        <div class="  hidden md:block">
+                            <BIcon icon="PhPhone" v-if="selectedChat.serviceType !== 'chat'"
+                                class="w-6 h-6 fill-on-surface/50 cursor-pointer" @click="handleAction('call')" />
                         </div>
+
+
+                        <BMenu ref="menuRef">
+                            <template #trigger>
+                                <BIcon icon="PhDotsThreeVertical" class="w-6 h-6 fill-on-surface/50 cursor-pointer"
+                                    aria-haspopup="true" aria-controls="overlay_menu" />
+                            </template>
+                            <div class=" flex flex-col gap-y-1 select-none w-60 p-3">
+                                <div @click="handleMenuAction(option.key)" v-for="option in menuOptions"
+                                    :key="option.key"
+                                    :class="[option.color && option.color === 'error' ? ' hover:bg-error/10 bg-error/0' : 'hover:bg-surface-variant-2 bg-surface-variant-2/0']"
+                                    class=" cursor-pointer group  transition-all duration-200 ease-in-out rounded-xl w-full flex items-center px-2.5 py-3 gap-x-2">
+                                    <BIcon
+                                        :class="[option.color && option.color === 'error' ? 'fill-error' : 'fill-on-surface/50 group-hover:fill-on-surface']"
+                                        class=" transition-all duration-200 ease-in-out w-5 h-5" weight="bold"
+                                        :icon="option.icon" />
+                                    <div class=" text-label-sm transition-all duration-200 ease-in-out"
+                                        :class="[option.color && option.color === 'error' ? 'text-error' : 'text-on-surface/50 group-hover:text-on-surface']">
+                                        {{ option.label }}</div>
+                                </div>
+                            </div>
+                        </BMenu>
                     </div>
-                </BMenu>
+                    <div :class="[!isSelectMode ? ' pointer-events-none opacity-0' : ' opacity-100 pointer-events-auto']" class=" flex items-center gap-x-4 ">
+                        <BIcon icon="PhTrash" class=" w-6 h-6 "
+                            :class="[canDelete ? 'fill-error cursor-pointer' : 'cursor-not-allowed fill-on-surface/50']" />
+                        <BIcon icon="PhCopy" class=" w-6 h-6 cursor-pointer fill-on-surface" />
+                    </div>
+                </div>
             </div>
         </div>
         <BIcon @click="goBack" icon="PhArrowLeft" class=" md:hidden fill-on-surface/50 w-6 h-6 cursor-pointer" />
@@ -49,7 +61,7 @@
 <script lang="ts">
 import { defineComponent, ref, computed, type PropType } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
-import { useChatStore, useI18n, useDate } from '#imports';
+import { useChatStore, useChatActionStore, useI18n, useDate } from '#imports';
 import type { Menu } from '~/types/components/menu';
 import type { Contact } from '~/types/chat';
 import ContactAvatar from './contact/ContactAvatar.vue';
@@ -67,6 +79,7 @@ export default defineComponent({
     },
     emits: ['call', 'open-profile'],
     setup(props, { emit }) {
+        const chatActionStore = useChatActionStore()
         const { formatRelativeDate } = useDate();
         const chatStore = useChatStore();
         const route = useRoute();
@@ -75,6 +88,8 @@ export default defineComponent({
         const menuRef = ref<Menu | null>(null)
         const selectedChat = computed(() => props.contact)
 
+        const isSelectMode = computed(() => chatActionStore.isSelectMode)
+        const canDelete = computed(() => chatActionStore.canDelete)
         // Template Ref for the Menu
 
 
@@ -159,7 +174,9 @@ export default defineComponent({
             handleMenuAction,
             handleAction,
             menuRef,
+            isSelectMode,
             goBack,
+            canDelete,
             openProfile,
         };
     }
