7da163ca2aad637aa361b452e9098cbecef8d8ff
Amir Safari <a.safari1380@gmail.com>
2026-04-29 10:04:12 +0330

    fixed the image upload component for the directive

--- app/components/general/ImageUpload.vue
+++ app/components/general/ImageUpload.vue
@@ -1,26 +1,18 @@
 <template>
   <div class="flex flex-col gap-y-2 w-full">
     <div class="p-4 w-full rounded-xl bg-surface-variant flex items-center gap-x-3 transition-all duration-200">
-      
-      <div 
-        v-image-picker="onInitialPick"
-        class="relative cursor-pointer bg-primary-container h-15 w-15 rounded-full flex items-center justify-center overflow-hidden transition-all duration-200"
-      >
+
+      <div v-image-pick="{ onSelect: onInitialPick }"
+        class="relative cursor-pointer bg-primary-container h-15 w-15 rounded-full flex items-center justify-center overflow-hidden transition-all duration-200">
         <template v-if="previewUrl">
-          <img 
-            :src="previewUrl" 
-            class="w-full h-full object-cover transition-opacity duration-200" 
-          />
-          <div class="absolute inset-0 bg-black/30 opacity-0 hover:opacity-100 transition-opacity duration-200 flex items-center justify-center">
+          <img :src="previewUrl" class="w-full h-full object-cover transition-opacity duration-200" />
+          <div
+            class="absolute inset-0 bg-black/30 opacity-0 hover:opacity-100 transition-opacity duration-200 flex items-center justify-center">
             <BIcon icon="PhCamera" class="w-6 h-6 fill-white" />
           </div>
         </template>
 
-        <BIcon 
-          v-else 
-          icon="PhUserPlus" 
-          class="w-6 h-6 fill-on-primary-container transition-opacity duration-200" 
-        />
+        <BIcon v-else icon="PhUserPlus" class="w-6 h-6 fill-on-primary-container transition-opacity duration-200" />
       </div>
 
       <div class="flex-1 select-none transition-all duration-200">
