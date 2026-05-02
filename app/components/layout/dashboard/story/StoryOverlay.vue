dcdfe2ea62457581dcaea506f4d8993a43ec43b6
Amir Safari <a.safari1380@gmail.com>
2026-05-02 07:29:22 +0330

    removed unused plugins

--- app/components/layout/dashboard/story/StoryOverlay.vue
+++ app/components/layout/dashboard/story/StoryOverlay.vue
@@ -37,7 +37,7 @@ export default defineComponent({
         return {
             storiesStore,
             closeStory,
-            opennedIndex,
+            opennedIndex,                                   
         }
     }
 })
--- app/plugins/emoji-picker.client.ts
+++
@@ -1,8 +0,0 @@
-// plugins/emoji-picker.client.ts
-export default defineNuxtPlugin(() => {
-  // We only import the side-effectful library here.
-  // It registers 'emoji-picker' to the customElements registry.
-  if (process.client) {
-    import('emoji-picker-element');
-  }
-});
